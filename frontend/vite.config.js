615e058dc14f400546f371949dce6eae5167fbd9
shrimon347 <shrimon1999@gmail.com>
2025-03-04 01:40:32 +0600

    feat(frontend,docker): add tailwind with shadcn and add volume in docker for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(path.dirname(new URL(import.meta.url).pathname), "./src"),
+    },
+  },
   server: {
-    host: '0.0.0.0',  
-    port: 5173,        
-  }
-})
+    host: "0.0.0.0",
+    port: 5173,
+  },
+});
